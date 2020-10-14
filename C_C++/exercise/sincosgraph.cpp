e117e5d1614496da937e530dc550be0277bb2ae6
Sirly <SirlyDreamer@users.noreply.github.com>
2020-10-14 16:10:29 +0800

    release

--- C_C++/exercise/sincosgraph.cpp
+++ C_C++/exercise/sincosgraph.cpp
@@ -4,27 +4,27 @@ using namespace std;
 int main()
 {
     int y(0);
-    char graph[27][65]{' '};
-    for (int i(0);i < 27;i ++)
+    char graph[128][27];
+    for (int i(0);i < 128;i ++)
     {
-        for (int j(0);j < 65;j++)
+        for (int j(0);j < 27;j++)
         {
-            if (i == 14)    
+            if (j == 14)
                 graph[i][j] = '-';
             else
                 graph[i][j] = ' ';
         }
     }
-    for (int x(0);x < 65;x ++)
+    for (int x(0);x < 128;x ++)
     {
-        y = (int)(13.0*sin(x) + 13.0);
+        y = (int)(13.0*sin(x/20.0) + 13.0);
         graph[x][y] = '*';
-        y = (int)(13.0*cos(x) + 13.0);
+        y = (int)(13.0*cos(x/20.0) + 13.0);
         graph[x][y] = '*';
     }
-    for (int i(27);i >= 0;i--)
+    for (int j(0);j < 27;j++)
     {
-        for (int j(0);j < 65;j++)
+        for (int i(127);i >= 0;i--)    
             cout << graph[i][j];
         cout << endl;
     }
