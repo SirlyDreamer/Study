4655c3c7b939b896e1d38a8813a703e81dd69607
Sirly <SirlyDreamer@users.noreply.github.com>
2020-10-20 07:55:47 +0800

    Release

--- C_C++/Study/LinkedList.cpp
+++ C_C++/Study/LinkedList.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <cstdlib>
 using namespace std;
 struct node
@@ -6,60 +6,91 @@ struct node
     int data;
     struct node *next;
 };
-struct node *head;
-struct node *temp1;
-struct node *temp2;
+struct node* head = NULL;
+struct node* tail = NULL;
 void addNode(int data)
 {
-    temp1 = (struct node*)malloc(sizeof(struct node));
-    temp1->data = data;
-    temp1->next = NULL;
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = NULL;
     if (head == NULL)
-        head = temp1;
+        head = newnode;
     else
-        temp2->next = temp1;
-    temp2 = temp1;
+        tail->next = newnode;
+    tail = newnode;
+    return;
 }
 void insertNode(int data,struct node *forward)
 {
-    temp1 = (struct node*)malloc(sizeof(struct node));
-    temp1->data = data;
-    temp1->next = forward->next;
-    forward->next = temp1;
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = forward->next;
+    forward->next = newnode;
+    return;
 }
-void deleteNode()
+void deleteNode(struct node* prev,struct node* place)
 {
-    //TO-DO
+    struct node* temp1 = prev;
+    struct node* temp2 = place;
+    temp1->next = temp2->next;
+    free(temp2);
+    return;
+}
+void deleteList(struct node* head)
+{
+    struct node* temp1 = head;
+    struct node* temp2 = head->next;
+    while (temp1 != NULL)
+    {
+        free(temp1);
+        temp1 = temp2;
+        if (temp2 != NULL)
+            temp2 = temp2->next;
+    }
+    return;
 }
 int main()
 {
-    head = NULL;
     for (int i=0;i<10;i++)
     {
-        printf("%d  ",i);
+        cout << i << ' ';
         addNode(i);
     }
     struct node *point;
     point = head;
-    printf("\n---------------------------------\n");
+    cout << endl << "---------------------------------" << endl;
     while (point != NULL)
     {
-        printf("%d  ",point->data);
+        cout << point->data << ' ';
         point = point->next;
     }
-    printf("\n---------------------------------\n");
+    cout << endl << "---------------------------------" << endl;
     int t;
-    scanf("%d",&t);
+    cin >> t;
     point = head;
     while (point->next != NULL && point->data < t)
         point = point->next;
     insertNode(t,point);
-    printf("\n---------------------------------\n");
+    cout << endl << "---------------------------------" << endl;
     point = head;
     while (point != NULL)
     {
-        printf("%d  ",point->data);
+        cout << point->data << ' ';
         point = point->next;
     }
+    cout << endl << "---------------------------------" << endl;
+    cin >> t;
+    point = head;
+    while (point->next != NULL && point->next->data != t)
+        point = point->next;
+    deleteNode(point,point->next);
+    cout << endl << "---------------------------------" << endl;
+    point = head;
+    while (point != NULL)
+    {
+        cout << point->data << ' ';
+        point = point->next;
+    }
+    deleteList(head);
     return 0;
 }
