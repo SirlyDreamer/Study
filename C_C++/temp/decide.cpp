98cf12d9adbea6c56e10f150ad78a4d696cc74ca
Sirly <SirlyDreamer@users.noreply.github.com>
2020-11-03 11:23:09 +0800

    gitignore

--- C_C++/temp/decide.cpp
+++ C_C++/temp/decide.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    for (int a = 1; a < 100; a++)
-        for (int b = 1; b < 100; b++)
-            for (int c = 1; c < 100; c++)
-                for (int d = 1; d < 100; d++)
-                    for (int f = 1; f < 100; f++)
+    for (int a = 3; a < 100; a++)
+        for (int b = 3; b < 100; b++)
+            for (int c = 3; c < 100; c++)
+                for (int d = 3; d < 100; d++)
+                    for (int f = 3; f < 100; f++)
                         if (23*a+24*b+67*c+45*d+34*f == 1000)
                             cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << f << endl;
     return 0;
