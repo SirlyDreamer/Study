17b062ec423a0ceb9985cb788786d44700e9aa35
Sirly <SirlyDreamer@users.noreply.github.com>
2020-11-12 23:14:24 +0800

    exercise

---
+++ C_C++/PKU/ArabPrince.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string add(string a,string b)
+{
+    string::iterator itera = a.end() - 1;
+    string::iterator iterb = b.end() - 1;
+    string result = "";
+    
+    while (itera != a.begin() && iterb != b.begin())
+    {
+        if (*itera + *iterb > '9')
+            *(iter-1) += 1
+    }
+}
+
+int main()
+{
+    
+}
---
+++ C_C++/PKU/TextAnnularEncrypt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<char,string> encode = {{' ',"00"},{'A',"01"},{'B',"02"}}
+
+int** encrypt(string str,int** matrix)
+{
+
+}
